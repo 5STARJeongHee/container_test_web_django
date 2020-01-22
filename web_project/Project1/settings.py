fca50b87313e4405ae00a38fdea6e46a37773cce
unknown <qhxmaoflr@gmail.com>
2020-01-22 13:40:59 +0900

    allowed_hosts all change

--- web_project/Project1/settings.py
+++ web_project/Project1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%7dw6n(j0+t^zvvoymg5v0j*mx250xt3b8vp@mwxwce^@lncbt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
